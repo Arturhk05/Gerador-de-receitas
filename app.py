034361d057bf4a105d4e8bc614ae36ade2c0dfb8
artur <arturkrauspenhar@hotmail.com>
2024-06-03 21:49:09 -0300

    Arrumando relacionamento de tabelas

--- app.py
+++ app.py
@@ -25,34 +25,47 @@ class ChatGPT(ABC):
         self.conexao = OpenAI(api_key = self.chave)
 
 class Ingrediente(db.Model):
+    __tablename__="ingredientes"
+
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String, unique=True)
-    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
 
     def __init__(self, nome):
         self.nome = nome
 
 class Usuario(db.Model):
+    __tablename__="usuarios"
+
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String, unique=True)
     senha = db.Column(db.String)
-    ingredientes = db.relationship('Ingrediente', backref='usuario')
+    ingredientes = db.relationship('Ingrediente', backref='usuarios')
 
     def __init__(self, nome, senha):
         self.nome = nome
         self.senha = senha
 
-class PaginaInicial():
-    def renderizar():
-        return render_template('index.html')
-
 @app.route("/")
 def index():
-    return PaginaInicial.renderizar()
+    users = Usuario.query.all()
+
+    return render_template('index.html', users=users)
 
 @app.route("/cadastro", methods=['POST', 'GET'])
 def cadastro():
-    return "oi"
+    if request.method == 'POST':
+        nome = request.form['nome']
+        senha = request.form['senha']
+
+        if nome == '' or senha == '' or nome == ' ' or senha == ' ':
+            return "Nome ou senha invalidos!"
+
+        user = Usuario(nome, senha)
+        db.session.add(user)
+        db.session.commit()
+        return "Usuário criado"
+    return render_template('usuario.html')
 
 if __name__ == "__main__":
     db.create_all()
